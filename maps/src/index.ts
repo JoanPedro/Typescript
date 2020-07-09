d410f5b5dc3b49acef9daf09f1330092be475dbb
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-07-09 09:24:30 -0300

    Add companyMarker in google maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,11 +1,11 @@
 import { Company } from './Company';
 import { User } from './User';
 import { CustomMap } from './CustomMap'
-// const user = new User();
-const company = new Company();
 
 // new CustomMap('map');
 const customMap = new CustomMap('map');
 const user = new User();
+const company = new Company();
 
 customMap.addUserMarker(user);
+customMap.addCompanyMarker(company);
