e4bb49927c5a9c3263098ced08726a4f43a4998f
Joan Pedro Oliveira de Souza <jpos.senai@gmail.com>
2020-07-13 14:12:20 -0300

    Ensure to print linkedList sorted values

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,12 +1,25 @@
+import { LinkedList } from './Collections/LinkedList';
 import { CharactersColletion } from './Collections/CharacteresCollections';
 import { NumbersCollections } from './Collections/NumberCollection';
 import { Sorted } from './Helper/Sorter';
 
 const numbersCollection = new NumbersCollections([-1, 7, 3, -15, 10, 9])
 const charactersCollection = new CharactersColletion('Xaayb')
+const linkedList = new LinkedList();
+
 const sortedNumberArray = new Sorted(numbersCollection)
 const sortedCharacterString = new Sorted(charactersCollection)
+const sortedLinkedList = new Sorted(linkedList)
+
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
 sortedNumberArray.sort()
 sortedCharacterString.sort()
+sortedLinkedList.sort()
+
 console.log(numbersCollection.data)
 console.log(charactersCollection.data)
+linkedList.print()
