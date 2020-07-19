aa65b6d0d7565264fbddee66c4a8ee7b80cd4e9a
Joan Pedro <jpos.senai@gmail.com>
2020-07-19 10:59:41 -0300

    Functions Generics

--- fetchjson/annotations/generics.ts
+++ fetchjson/annotations/generics.ts
@@ -22,6 +22,46 @@ class ArrayOfAnything<Type> {
   }
 }
 
-new ArrayOfAnything<string>(['a', 'b', 'c'])
-new ArrayOfAnything<number>([1, 2, 6])
-const arr = new ArrayOfAnything(['a', 'b', 'c'])
+new ArrayOfAnything<string>(['a', 'b', 'c']);
+new ArrayOfAnything<number>([1, 2, 6]);
+const arr = new ArrayOfAnything(['a', 'b', 'c']);
+
+// Generics with functions:
+
+function printStrings(arr: string[]): void {
+  printAnything(arr);
+}
+
+const printStringsArrow = (arr: string[]): void => {
+  printAnythingArrow(arr);
+}
+
+function printNumbers(arr: number[]): void {
+  printAnything(arr);
+}
+
+const printNumbersArrow = (arr: number[]): void => {
+  printAnythingArrow(arr);
+}
+
+function printAnything<Type>(arr: Type[]): void {
+  arr.forEach(elem => console.log(elem));
+}
+
+const printAnythingArrow = <Type>(arr: Type[]): void => {
+  arr.forEach(elem => console.log(elem));
+}
+
+printAnything<string>(['a', 'b']);
+printStrings(['c', 'd']);
+
+printAnything<number>([4, 5, 6]);
+printNumbers([1, 2, 3]);
+
+// Arrow Function
+
+printAnythingArrow<string>(['a', 'b']);
+printStringsArrow(['c', 'd']);
+
+printAnythingArrow<number>([4, 5, 6]);
+printNumbersArrow([1, 2, 3]);
