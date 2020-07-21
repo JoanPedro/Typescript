f2977433b0ace10d5af9dfff0e2cbe87147dcb68
Joan Pedro <jpos.senai@gmail.com>
2020-07-21 09:29:31 -0300

    Retrieving User Properties

--- webFramework/src/index.ts
+++ webFramework/src/index.ts
@@ -1,1 +1,7 @@
-console.log('Hi there')
+import { User } from './models/User'
+
+const user = new User({name: 'Joan Pedro', age: 20});
+console.log(`
+  Nome: ${user.get('name')},
+  Idade: ${user.get('age')}
+`);
