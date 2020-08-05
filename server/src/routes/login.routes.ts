2de9f130e278176b473d2724a71cbd5ef27a7f13
Joan Pedro <jpos.senai@gmail.com>
2020-08-05 09:10:24 -0300

    Move LoginController to its own file

--- server/src/routes/login.routes.ts
+++ server/src/routes/login.routes.ts
@@ -17,24 +17,6 @@ const requireAuth = (req: Request, res: Response, next: NextFunction): void => {
 
 const router = Router();
 
-router.get('/login', (req: Request, res: Response): Response => {
-  return res.send(
-    `
-      <form method="POST">
-        <div>
-          <label> Email </label>
-          <input name="email" />
-        </div>
-        <div>
-          <label> Password </label>
-          <input name="password" type="password" />
-        </div>
-        <button> Submit </button>
-      </form>
-    `
-  )
-})
-
 router.post('/login', (req: Request, res: Response): void => {
   try {
     const { email, password }: Login = req.body;
@@ -86,4 +68,5 @@ router.get('/logout', (req: Request, res: Response): void => {
 router.get('/protected', requireAuth, (req: Request, res: Response): Response => {
   return res.send({ msg: 'Welcome to protected route, logged user' })
 })
+
 export { router }
